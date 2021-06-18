933abd36f1779daf99040f098ba4a19acf0d509f
star0202 <61998307+star0202@users.noreply.github.com>
2021-06-18 13:03:13 +0900

    Update image.js

--- asset/js/image.js
+++ asset/js/image.js
@@ -1,13 +1,14 @@
-var imageURLs = [
-    "/asset/image/1.gif"
-  , "/asset/image/2.gif"
-  , "/asset/image/3.gif"
-  , "/asset/image/4.gif"
+var imageURLs
+imageURLs = [
+  "/asset/image/1.gif"
+, "/asset/image/2.gif"
+, "/asset/image/3.gif"
+, "/asset/image/4.gif"
 ];
 function getImageTag() {
- var img = '<img src=\"';
- var randomIndex = Math.floor(Math.random() * imageURLs.length);
- img += imageURLs[randomIndex];
- img += '\" alt=\"Cute!!\"/>';
- return img;
+  var img = '<img src=\"';
+  var randomIndex = Math.floor(Math.random() * imageURLs.length);
+  img += imageURLs[randomIndex];
+  img += '\" alt=\"Cute!!\"/>';
+  return img;
 }
